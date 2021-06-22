0758a3acd5b99fc76da357eb37ec6508189114e6
treenate <32534779+treenate@users.noreply.github.com>
2021-06-22 18:56:34 +0700

    Create jenkins-connect.js

---
+++ jenkins-connect.js
@@ -0,0 +1,159 @@
+var Header = Java.type('org.apache.http.Header');
+var URI = Java.type('java.net.URI');
+
+var HttpEntity = Java.type('org.apache.http.HttpEntity');
+var HttpHost = Java.type('org.apache.http.HttpHost');
+var HttpResponse = Java.type('org.apache.http.HttpResponse');
+var AuthScope = Java.type('org.apache.http.auth.AuthScope');
+var UsernamePasswordCredentials = Java.type('org.apache.http.auth.UsernamePasswordCredentials');
+var AuthCache = Java.type('org.apache.http.client.AuthCache');
+var ClientProtocolException = Java.type('org.apache.http.client.ClientProtocolException');
+var CredentialsProvider = Java.type('org.apache.http.client.CredentialsProvider');
+var HttpPost = Java.type('org.apache.http.client.methods.HttpPost');
+var HttpClientContext = Java.type('org.apache.http.client.protocol.HttpClientContext');
+var BasicScheme = Java.type('org.apache.http.impl.auth.BasicScheme');
+var BasicAuthCache = Java.type('org.apache.http.impl.client.BasicAuthCache');
+var BasicCredentialsProvider = Java.type('org.apache.http.impl.client.BasicCredentialsProvider');
+var CloseableHttpClient = Java.type('org.apache.http.impl.client.CloseableHttpClient');
+var HttpClients = Java.type('org.apache.http.impl.client.HttpClients');
+var EntityUtils = Java.type('org.apache.http.util.EntityUtils');
+
+function JenkinsConnector(urlJenkins, userName, authToken) {
+    this._urlJenkins = urlJenkins;
+    this._userName = userName;
+    this._authToken = authToken;
+    this._userPassCred = new UsernamePasswordCredentials(this._userName, this._authToken);
+
+    this.remoteBuild = function(jobName , params ,token) {
+        var queueID = '';
+        var httpClient;
+        try {
+            
+            var urlJob = `${this._urlJenkins}/job/${jobName}`;
+            //buildWithParameters?token=bdk-token
+            if(params != null){
+                var urlParam = Object.keys(params).map(function(k) {
+                    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+                }).join('&')
+                urlJob += '/buildWithParameters'+(token != null ? '?token='+token +'&': '?');
+                urlJob += urlParam;       
+            }else{
+                urlJob += '/build'+(token != null ? '?token='+token : '');
+            }
+            var uri = URI.create(urlJob);
+            var host = new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+
+            var credsProvider = new BasicCredentialsProvider();
+            credsProvider.setCredentials(new AuthScope(uri.getHost(), uri.getPort()), this._userPassCred);
+            var authCache = new BasicAuthCache();
+            var basicAuth = new BasicScheme();
+            authCache.put(host, basicAuth);
+
+            httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+            var httpPost = new HttpPost(uri);
+            var localContext = HttpClientContext.create();
+            localContext.setAuthCache(authCache);
+
+            var response = httpClient.execute(host, httpPost, localContext);
+
+            var regex = /queue\/item\/(\d{1,})?\//i;
+            for (var i = 0; i < response.getAllHeaders().length; i++) {
+                var header = response.getAllHeaders()[i];
+                var name = header.getName();
+                var value = header.getValue();
+                if (name == 'Location') {
+                    var m;
+                    if ((m = regex.exec(value)) !== null) {
+                        queueID = m[1];
+                        break;
+                    }
+                }
+            }
+
+            httpClient.close();
+
+        } catch (err) {
+            if (httpClient != null) {
+                httpClient.close();
+            }
+            throw err;
+        }
+        return queueID;
+    }
+
+    this.getQueueItem= function( queueID) {
+        var ret = { executable : false };
+        var httpClient;
+        try {
+            var uri = URI.create(`urlJenkins/queue/item/${queueID}/api/json?pretty=true`);
+            var host = new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+
+            var credsProvider = new BasicCredentialsProvider();
+            credsProvider.setCredentials(new AuthScope(uri.getHost(), uri.getPort()), this._userPassCred);
+            var authCache = new BasicAuthCache();
+            authCache.put(host, new BasicScheme());
+
+            httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+            var httpGet = new HttpGet(uri);
+            var localContext = HttpClientContext.create();
+            localContext.setAuthCache(authCache);
+
+            var response = httpClient.execute(host, httpGet, localContext);
+            var statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() >= 300 ){
+                throw 'Request fail.'
+            }
+            var responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+            if(responseBody.executable){
+                ret.executable = true;
+                ret.jobID = responseBody.executable.number;
+            }
+            httpClient.close();
+
+        } catch (err) {
+            if (httpClient != null) {
+                httpClient.close();
+            }
+            throw err;
+        }
+        return ret;
+    }
+    
+    this.getJobItem= function(jobName,jobID) {
+        var ret = { };
+        var httpClient;
+        try {
+            var uri = URI.create(`urlJenkins/job/${jobName}/${jobID}/api/json?pretty=true`);
+            var host = new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+
+            var credsProvider = new BasicCredentialsProvider();
+            credsProvider.setCredentials(new AuthScope(uri.getHost(), uri.getPort()), this._userPassCred);
+            var authCache = new BasicAuthCache();
+            authCache.put(host, new BasicScheme());
+
+            httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+            var httpGet = new HttpGet(uri);
+            var localContext = HttpClientContext.create();
+            localContext.setAuthCache(authCache);
+
+            var response = httpClient.execute(host, httpGet, localContext);
+            var statusLine = response.getStatusLine();
+            if(statusLine.getStatusCode() >= 300 ){
+                throw 'Request fail.'
+            }
+            var responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+            ret = responseBody;
+            httpClient.close();
+
+        } catch (err) {
+            if (httpClient != null) {
+                httpClient.close();
+            }
+            throw err;
+        }
+        return ret;
+    }
+
+}
+
+export default JenkinsConnector
